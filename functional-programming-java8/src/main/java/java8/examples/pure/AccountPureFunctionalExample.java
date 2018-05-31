846584bdd4924dec1ebe551357d09447f076ddcb
Yogesh Naik <yogeshrnaik@gmail.com>
2018-05-31 13:16:43 +0530

    refactoring

--- functional-programming-java8/src/main/java/java8/examples/pure/AccountPureFunctionalExample.java
+++ functional-programming-java8/src/main/java/java8/examples/pure/AccountPureFunctionalExample.java
@@ -1,16 +1,37 @@
 package java8.examples.pure;
 
+import io.vavr.control.Try;
+
 public class AccountPureFunctionalExample {
 
     public static void main(String[] args) {
+        impureDepositeExample();
+        pureDepositExample();
+        pureWithdrawExample();
+    }
+
+    private static void pureWithdrawExample() {
+        System.out.println("******************** Pure Function - Withdraw ***************** ");
+        AccountPure currAcct = new AccountPure(1000);
+        // currAcct.withdraw(2000);
+
+        Double balance = currAcct.tryWithdraw(2000)
+            .map(AccountPure::getBalance)
+            .getOrElseThrow(t -> new RuntimeException(t));
+        System.out.println(balance);
+    }
+
+    private static void impureDepositeExample() {
         Account acc = new Account(1000);
         System.out.println(acc.balance);
         acc.deposit(500);
         System.out.println(acc.balance);
+    }
 
-        System.out.println("******************** Pure Function ***************** ");
+    private static void pureDepositExample() {
+        System.out.println("******************** Pure Function - Deposit ***************** ");
         AccountPure currAcct = new AccountPure(1000);
-        Account newAccState = currAcct.deposit(500);
+        AccountPure newAccState = currAcct.deposit(500);
         System.out.println(currAcct.balance);
         System.out.println(newAccState.balance);
     }
@@ -25,11 +46,9 @@ class Account {
     }
 
     public double deposit(double amount) {
-        // state changes hence not pure
         balance += amount;
         return balance;
     }
-
 }
 
 class AccountPure {
@@ -40,8 +59,33 @@ class AccountPure {
         this.balance = balance;
     }
 
-    public Account deposit(double amount) {
+    public AccountPure deposit(double amount) {
         // state of Account object is not changed. we create Account with new balance
-        return new Account(balance + amount);
+        return new AccountPure(balance + amount);
+    }
+
+    public AccountPure withdraw(double amount) {
+        if (balance - amount < 0)
+            throw new InsufficientBalance(String.format("Cannot withdraw %.2f. Current balance: %.2f", amount, balance));
+        return new AccountPure(balance - amount);
+    }
+
+    public Try<AccountPure> tryWithdraw(double amount) {
+        if (balance - amount < 0)
+            return Try.failure(new InsufficientBalance(String.format("Cannot withdraw %.2f. Current balance: %.2f", amount, balance)));
+        return Try.success(new AccountPure(balance - amount));
+    }
+
+    public static double getBalance(AccountPure acc) {
+        return acc.balance;
+    }
+}
+
+class InsufficientBalance extends RuntimeException {
+
+    private static final long serialVersionUID = -2106051575900723379L;
+
+    public InsufficientBalance(String message) {
+        super(message);
     }
 }
